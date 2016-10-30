f9114db1fe65b13c88cc028001ba0b6832f208d5
“dotnot2014git config --global user.email “5168247@qq.com <5168247@qq.com>
2016-10-30 21:18:45 +0800

    添加了热门频道的实现

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -3,10 +3,85 @@
 var app = getApp()
 Page({
   data: {
-      title:"热门图文"
+    articles:[],
+    eachdatas:[],
+    hidden:true,
+    hasmore:true,
+    moredata:0
+  },
+  // 数据加载通用函数
+  loadData: function(howmore){
+      var that = this
+      wx.request({
+      url: 'http://app.admin.weijuju.com/mobile/article/getArticleList',
+      data: {
+        order:"asc",
+        limit:10,
+        offset:howmore
+      },
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res) {
+
+        for(var i=0;i<res.data.rows.length;i++){
+          res.data.rows[i].postTime = res.data.rows[i].postTime.split(" ")[0]
+        }
+
+        if(howmore){
+          that.setData({
+            articles: that.data.articles.concat(res.data.rows)
+          })
+        }else{
+          that.setData({
+            articles: res.data.rows
+          })
+        }
+      }
+    })
+  },
+  // 到页面底部，加载更多数据
+  loadMore: function(){
+    var that = this
+
+    that.setData({
+      hasmore: false,
+      moredata: that.data.moredata + 10
+    })
+
+    setTimeout(function () {
+          that.loadData(that.data.moredata)
+          that.setData({hasmore: true})
+          console.log('数据已加载！！！...')
+        }, 650)
+  },
+  // loading变化后处理
+  loadingChange: function(){
+    this.setData({
+      hidden: true
+    })
+  },
+  //Tap事件，初始页面调整处理（seedetails）
+  seedetails: function(e) {
+    wx.setStorageSync('articles', this.data.articles)
+    wx.navigateTo({
+      url: '../details/details?id='+e.currentTarget.id
+    })
+  },
+  // 下拉刷新处理
+  onPullDownRefresh: function() {
+    var that = this
+    that.setData({hidden: false})
+    setTimeout(function () {
+          that.loadData(0)
+          that.setData({hidden: true})
+          console.log('数据已加载...')
+          wx.stopPullDownRefresh()
+        }, 650)
   },
   // 应用启动时加载数据
-  onLoad: function (options) {
-    //   do something
+  onLoad: function () {
+    var that = this
+    that.loadData(0)
   }
 })
--- pages/subscribe/subscribe.js
+++ pages/subscribe/subscribe.js
@@ -1,14 +1,55 @@
-//subscribe.js
-//获取应用实例
-var util = require('../../utils/util.js')
+// //subscribe.js
+// //获取应用实例
+// var util = require('../../utils/util.js')
+// var app = getApp()
+// Page({
+//   data: {
+//       title:"订阅图文",
+//       content:"",
+//   },
+//   // 应用启动时加载数据
+//   onLoad: function () {
+//     // do something
+//   }
+// })
+
+
 var app = getApp()
 Page({
   data: {
-      title:"订阅图文",
-      content:"",
+    hasUserInfo: false
   },
-  // 应用启动时加载数据
-  onLoad: function () {
-    // do something
+  getUserInfo: function () {
+    var that = this
+
+    if (app.globalData.hasLogin === false) {
+      wx.login({
+        success: _getUserInfo
+      })
+    } else {
+      _getUserInfo()
+    }
+
+    function _getUserInfo() {
+      wx.getUserInfo({
+        success: function (res) {
+          that.setData({
+            hasUserInfo: true,
+            userInfo: res.userInfo
+          })
+          that.update()
+        }
+      })
+    }
+  },
+   onLoad: function () {
+    var that = this
+    that.getUserInfo()
+  },
+  clear: function () {
+    this.setData({
+      hasUserInfo: false,
+      userInfo: {}
+    })
   }
 })
